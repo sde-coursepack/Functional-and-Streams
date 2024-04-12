817e22012f5ba0eb90177391aee6cdadf6bff0a5
WillMcBurney <pmcburn1@gmail.com>
2024-04-12 08:45:27 -0400

    Removed array usage and just use List

--- src/main/java/mcburney/edu/functional/sorting/SortingDemo.java
+++ src/main/java/mcburney/edu/functional/sorting/SortingDemo.java
@@ -10,17 +10,18 @@ import java.util.List;
 
 public class SortingDemo {
     public static void main(String[] args) {
-        String[] words = {"To be or not to be?, That is the question", "Whether tis nobler in the mind to suffer",
-        "The slings and arrows of outrageous fortune", "or to takes arms against a sea of troubles",
-        "and by opposing, end them. To die, to sleep--", "No more--and by a sleep to say we end",
-        "the heartache, and the thousand natural shocks", "That flesh is heir to. 'Tis a consummation",
-        "Devoutly to be wished. To die, to sleep", "To sleep perchance to dream: ay, there's the rub."};
+        List<String> soliloquoy = List.of(
+                "To be or not to be?, That is the question", 
+                "Whether tis nobler in the mind to suffer", 
+                "The slings and arrows of outrageous fortune", 
+                "or to takes arms against a sea of troubles", 
+                "and by opposing, end them. To die, to sleep--", 
+                "No more--and by a sleep to say we end", 
+                "the heartache, and the thousand natural shocks", 
+                "That flesh is heir to. 'Tis a consummation", 
+                "Devoutly to be wished. To die, to sleep", 
+                "To sleep perchance to dream: ay, there's the rub.");
 
-       Arrays.sort(words); //Sort by the default String compareTo method
-
-
-        List<String> wordList = Arrays.asList(words);
-
-        wordList.forEach(System.out::println);
+        soliloquoy.forEach(System.out::println);
     }
 }
